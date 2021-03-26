fa641ece7d20a95dadfd685d536c81fcdb7a1e68
Mattia Sanfilippo <imattolo@me.com>
2021-03-26 20:49:58 +0100

    feat: add reactotron

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * @format
- */
-
+if (__DEV__) {
+  import('./reactotronConfig').then(() =>
+    console.log('Reactotron Initialized'),
+  );
+}
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
