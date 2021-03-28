d2a5e769a69aa8754276456377f7e052f3ef3b2e
Mattia Sanfilippo <imattolo@me.com>
2021-03-28 12:48:26 +0200

    style: cleanup

--- App.tsx
+++ App.tsx
@@ -1,16 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {Home} from './src/home/views/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from './src/home/views/Home';
 
 const App = () => {
   const Stack = createStackNavigator();
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ if (__DEV__) {
     console.log('Reactotron Initialized'),
   );
 }
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
